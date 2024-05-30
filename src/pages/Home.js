9c604f40d2b734ca4f053ddf97365b38b1fc996b
jamal019 <j.abdallah19@outlook.de>
2024-05-30 12:57:15 +0200

    Version 0.1

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+// src/pages/Home.js
+import React from 'react';
+import SwipeComponent from '../components/SwipeComponent';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Watchflow</h1>
+      <SwipeComponent />
+    </div>
+  );
+};
+
+export default Home;
