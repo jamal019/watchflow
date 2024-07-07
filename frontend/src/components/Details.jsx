acfe32ea94a5442fba216f8166eb99a0c40fefed
Hamzah Fayad <hamzahfayad99@gmail.com>
2024-07-07 15:38:39 +0200

    movie trailer

--- frontend/src/components/Details.jsx
+++ frontend/src/components/Details.jsx
@@ -3,6 +3,7 @@ import "./Details.css";
 
 const Details = ({ movieId }) => {
   const [movieDetails, setMovieDetails] = useState(null);
+  const [trailerKey, setTrailerKey] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -31,6 +32,19 @@ const Details = ({ movieId }) => {
         setError(error);
         setLoading(false);
       });
+       // Fetch movie trailers
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, options)
+    .then((response) => response.json())
+    .then((data) => {
+      const trailers = data.results.filter(video => video.site === "YouTube" && video.type === "Trailer");
+      if (trailers.length > 0) {
+        setTrailerKey(trailers[0].key);
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching movie trailers:", error);
+      setError(error);
+    });
   }, [movieId, options]);
 
   if (loading) {
@@ -45,13 +59,37 @@ const Details = ({ movieId }) => {
     <section className="details-page">
       {movieDetails && (
         <>
-          <img
-            className="modal-details-img"
-            src={" https://image.tmdb.org/t/p/w500/" + movieDetails.poster_path}
-            alt=""
-          />
+          {trailerKey ? (
+            <div className="modal-details-video-container">
+            <iframe
+              className="modal-details-video"
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${trailerKey}`}
+              title="YouTube video player"
+              autoPlay="1"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            </div>
+          ) : (
+            <img
+              className="modal-details-img"
+              src={"https://image.tmdb.org/t/p/w500/" + movieDetails.poster_path}
+              alt=""
+            />
+          )}
           <div className="modal-details-text">
             <h2>{movieDetails.title}</h2>
+            <div className="genres">
+            {
+              movieDetails.genres.map((genre) => {
+               return <p><strong>{genre.name}</strong></p>
+              })
+            }
+            </div>
+            <br/>
             <p>{movieDetails.release_date.split("-")[0]}</p>
             <br />
             <p>{movieDetails.overview}</p>
